54ad6d6566c320f7ba9a800c65e0359364eeedf9
Justin Clark <justin.clark.git@gmail.com>
2024-12-13 09:10:02 -0500

    functioning - now just need to prettify a bit

--- app/app.py
+++ app/app.py
@@ -23,39 +23,71 @@ sys.path.append('../')
 from extract_bottleneck_features import extract_Resnet50
 
 
-dog_names = [item[20:-1] for item in sorted(glob("../dogImages/train/*/"))]
-
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'static/uploads/'
 
-# Ensure the upload folder exists
-os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+UPLOAD_FOLDER = 'static/uploads/'
+
+# define folder to hold images the user uplaods
+app.config['USER_IMAGE_UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# create that folder, if it doesn't exist
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 ImageFile.LOAD_TRUNCATED_IMAGES = True                 
+
+#################################################################################
+##### Define items needed for model to make predictions on arbitrary images #####
+#################################################################################
+
+dog_names = [item[20:-1] for item in sorted(glob("../dogImages/train/*/"))] 
 
 # extract pre-trained face detector
 face_cascade = cv2.CascadeClassifier('../haarcascades/haarcascade_frontalface_alt.xml')
 
-
 # define ResNet50 model
 ResNet50_model = ResNet50(weights='imagenet')
 
-
-# returns "True" if face is detected in image stored at img_path
 def face_detector(img_path):
+    """ returns "True" if face is detected in image stored at img_path.
+
+    Args:
+        img_path (str): Path to image file.
+
+    Returns:
+        bool: True if a face is detected, else False.
+    """
     img = cv2.imread(img_path)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray)
     return len(faces) > 0
 
-
-### returns "True" if a dog is detected in the image stored at img_path
 def dog_detector(img_path):
+    """ returns "True" if dog is detected in image stored at img_path.
+
+    Args:
+        img_path (str): Path to image file.
+
+    Returns:
+        bool: True if a dog is detected, else False.
+    """
+    def ResNet50_predict_labels(img_path):
+        ### returns prediction vector for image located at img_path ###
+        img = preprocess_input(path_to_tensor(img_path))
+        return np.argmax(ResNet50_model.predict(img, verbose=False))
+
     prediction = ResNet50_predict_labels(img_path)
     return ((prediction <= 268) & (prediction >= 151)) 
 
 
-def path_to_tensor(img_path):    
+def path_to_tensor(img_path):
+    """ Load an image and convert to a 4D tensor.
+
+    Args:
+        img_path (str): Path to image file.
+
+    Returns:
+        np.array: 4d tensor array.
+    """
     # loads RGB image as PIL.Image.Image type
     img = image.load_img(img_path, target_size=(224, 224))
     # convert PIL.Image.Image type to 3D tensor with shape (224, 224, 3)
@@ -69,77 +101,48 @@ def paths_to_tensor(img_paths):
     return np.vstack(list_of_tensors)
 
 
-def ResNet50_predict_labels(img_path):
-    # returns prediction vector for image located at img_path
-    img = preprocess_input(path_to_tensor(img_path))
-    return np.argmax(ResNet50_model.predict(img, verbose=False))
-
-
-def predict_breed_from_image(img_path, model):
-    """ Predict dog breed from image,
+def classify_image(img_path, model):
+    """ Wrapper function to predict dog breed from an image using supplied model.
+    The function will attempt to detect both dogs and people in the image. In either
+    case, it will generate a predicted breed that the dog or person most resembles.
 
     Args:
-        img_path (_type_): Path to image file.
+        img_path (_tstrype_): Path to image file.
+        model (object): Model used to make prediction.
 
     Returns:
-        str: Predicted breed from image.
+        str: String containing a message including the dog breed detected.
     """
-    # extract bottleneck features
-    bottleneck_feature = extract_Resnet50(path_to_tensor(img_path))
-    # obtain predicted vector
-    predicted_vector = model.predict(bottleneck_feature)
-    # return dog breed that is predicted by the model
-    return dog_names[np.argmax(predicted_vector)]    
+    
+    def predict_breed(img_path, model):
+        """ Predict dog breed from image.
 
+        Args:
+            img_path (_tstrype_): Path to image file.
+            model (object): Model used to make prediction.
 
-def classify_image(img_path, model):
-    # dog detected...
-    is_dog = dog_detector(img_path)
-
-    # person detected...
-    is_human = face_detector(img_path)
+        Returns:
+            str: Predicted breed from image.
+        """
+        # extract bottleneck features
+        bottleneck_feature = extract_Resnet50(path_to_tensor(img_path))
+        # obtain predicted vector
+        predicted_vector = model.predict(bottleneck_feature)
+        # return dog breed that is predicted by the model
+        return dog_names[np.argmax(predicted_vector)]    
+    
+    is_dog = dog_detector(img_path) # dog detected
+    is_human = face_detector(img_path) # person detected
 
     if is_dog or is_human:
-        breed = predict_breed_from_image(img_path, model)
+        breed = predict_breed(img_path, model)
         if is_dog:
-            answer = f'{img_path} contains a dog that looks like a: {breed}'
+            answer = f'This image contains a dog that looks like: {breed}'
         else:
-            answer = f'{img_path} contains a person who looks like a: {breed}'
+            answer = f'This image contains a person who looks like: {breed}'
     else:
-        print(f'No dog or human was detected in this image: {img_path}')
-        answer = -1
+        answer = 'No dog or human was detected in this image.'
     return answer
-
-
-# def load_data_train_model():
-#     def load_dataset(path):
-#         data = load_files(path)
-#         dog_files = np.array(data['filenames'])
-#         dog_targets = np_utils.to_categorical(np.array(data['target']), 133)
-#         return dog_files, dog_targets
-
-#     # load train, test, and validation datasets
-#     train_files, train_targets = load_dataset('../dogImages/train')
-#     valid_files, valid_targets = load_dataset('../dogImages/valid')
-#     test_files, test_targets = load_dataset('../dogImages/test')
-
-#     bottleneck_features = np.load('../bottleneck_features/DogResNet50Data.npz')
-#     train_justinBottle = bottleneck_features['train']
-#     valid_justinBottle = bottleneck_features['valid']
-
-#     justinBottle_model = models.Sequential()
-#     justinBottle_model.add(layers.GlobalAveragePooling2D(input_shape=train_justinBottle.shape[1:]))
-#     justinBottle_model.add(layers.Dense(133, activation='softmax'))
-#     justinBottle_model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
-#     checkpointer = ModelCheckpoint(filepath='../saved_models/weights.best.justinBottle.hdf5', 
-#                                 verbose=1, save_best_only=True)
-
-#     justinBottle_model.fit(train_justinBottle, train_targets, 
-#             validation_data=(valid_justinBottle, valid_targets),
-#             epochs=5, batch_size=20, callbacks=[checkpointer], verbose=1)
-#     justinBottle_model.load_weights('../saved_models/weights.best.justinBottle.hdf5')
-
-#     return justinBottle_model
 
 
 def load_justin_model(model_save_file='../saved_models/justin_model'):
@@ -165,10 +168,10 @@ def upload_file():
         return redirect(request.url)
     if file:
         filename = secure_filename(file.filename)
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file_path = os.path.join(app.config['USER_IMAGE_UPLOAD_FOLDER'], filename)
         file.save(file_path)
         
-        print(file_path)
+        # After the user uploads the file, predict the breed from it
         breed = classify_image(file_path, justinBottle_model)
         print(breed)
 
@@ -176,8 +179,14 @@ def upload_file():
         image = Image.open(file_path)
         image_info = f"Image size: {image.size[0]}x{image.size[1]}"
         
-        # Create a simple Plotly figure as an example
-        fig = px.imshow(image)
+        # Display the image the user uploaded
+        fig = px.imshow(image, title=breed)
+
+        # hide the axis tick labels
+        fig.update_layout(
+            xaxis={'showticklabels': False},
+            yaxis={'showticklabels': False},
+        )
         plot_html = pio.to_html(fig, full_html=False)
         
         return render_template(
